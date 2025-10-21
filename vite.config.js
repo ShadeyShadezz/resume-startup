0fd71784fa2fb4500d5bb2017cdd0f0397a00c7c
William <Shadowsteampunk@gmail>
2025-10-21 09:35:56 -0400

    mv App.js and main.jsx from / to src/

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // Configure Vite
 export default defineConfig({
   plugins: [react()],
-  root: "career-lab", // 👈 this points Vite to your index.html
-  server: {
-    open: true,
-    port: 5173, // optional: choose your port
-  },
+  
 });
